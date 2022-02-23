f8eda0183e8360d2c3a084b82c10eb6b529dab2b
Prem Saraswat <prmsrswt@gmail.com>
2022-02-23 07:14:21 +0530

    Implement login using client credentials grant flow
    Signed-off-by: Prem Saraswat <prmsrswt@gmail.com>

--- go.mod
+++ go.mod
@@ -10,9 +10,17 @@ require (
 )
 
 require (
+	github.com/coreos/go-oidc/v3 v3.1.0 // indirect
 	github.com/efficientgo/tools/core v0.0.0-20210609125236-d73259166f20 // indirect
 	github.com/go-kit/kit v0.10.0 // indirect
 	github.com/go-logfmt/logfmt v0.5.1 // indirect
+	github.com/golang/protobuf v1.4.3 // indirect
 	github.com/inconshreveable/mousetrap v1.0.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
+	golang.org/x/crypto v0.0.0-20200622213623-75b288015ac9 // indirect
+	golang.org/x/net v0.0.0-20210525063256-abc453219eb5 // indirect
+	golang.org/x/oauth2 v0.0.0-20210514164344-f6687ab2804c // indirect
+	google.golang.org/appengine v1.6.6 // indirect
+	google.golang.org/protobuf v1.26.0-rc.1 // indirect
+	gopkg.in/square/go-jose.v2 v2.5.1 // indirect
 )
--- pkg/cmd/login.go
+++ pkg/cmd/login.go
@@ -2,28 +2,106 @@ package cmd
 
 import (
 	"context"
+	"fmt"
+	"net/url"
 
-	"github.com/go-kit/log/level"
+	"github.com/coreos/go-oidc/v3/oidc"
+	"github.com/go-kit/log"
 	"github.com/spf13/cobra"
+	"golang.org/x/oauth2/clientcredentials"
+
+	"github.com/observatorium/obsctl/pkg/config"
 )
 
+type loginConfig struct {
+	tenant string
+	api    string
+	ca     string
+	oidc   struct {
+		issuerURL    string
+		clientID     string
+		clientSecret string
+		audience     string
+	}
+}
+
 func NewLoginCmd(ctx context.Context) *cobra.Command {
+	var cfg loginConfig
+
 	cmd := &cobra.Command{
 		Use:   "login",
 		Short: "Login as a tenant. Will also save tenant details locally.",
 		Long:  "Login as a tenant. Will also save tenant details locally.",
-		Run: func(cmd *cobra.Command, args []string) {
-			level.Info(logger).Log("msg", "login called")
+		RunE: func(cmd *cobra.Command, args []string) error {
+			return runLogin(ctx, logger, cfg)
 		},
 	}
 
-	cmd.Flags().String("tenant", "", "The name of the tenant.")
-	cmd.Flags().String("api", "", "The URL or name of the Observatorium API.")
-	cmd.Flags().String("ca", "", "Path to the TLS CA against which to verify the Observatorium API. If no server CA is specified, the client will use the system certificates.")
-	cmd.Flags().String("oidc.issuer-url", "", "The OIDC issuer URL, see https://openid.net/specs/openid-connect-discovery-1_0.html#IssuerDiscovery.")
-	cmd.Flags().String("oidc.client-secret", "", "The OIDC client secret, see https://tools.ietf.org/html/rfc6749#section-2.3.")
-	cmd.Flags().String("oidc.client-id", "", "The OIDC client ID, see https://tools.ietf.org/html/rfc6749#section-2.3.")
-	cmd.Flags().String("oidc.audience", "", "The audience for whom the access token is intended, see https://openid.net/specs/openid-connect-core-1_0.html#IDToken.")
+	cmd.Flags().StringVar(&cfg.tenant, "tenant", "", "The name of the tenant.")
+	cmd.Flags().StringVar(&cfg.api, "api", "", "The URL or name of the Observatorium API.")
+	cmd.Flags().StringVar(&cfg.ca, "ca", "", "Path to the TLS CA against which to verify the Observatorium API. If no server CA is specified, the client will use the system certificates.")
+	cmd.Flags().StringVar(&cfg.oidc.issuerURL, "oidc.issuer-url", "", "The OIDC issuer URL, see https://openid.net/specs/openid-connect-discovery-1_0.html#IssuerDiscovery.")
+	cmd.Flags().StringVar(&cfg.oidc.clientSecret, "oidc.client-secret", "", "The OIDC client secret, see https://tools.ietf.org/html/rfc6749#section-2.3.")
+	cmd.Flags().StringVar(&cfg.oidc.clientID, "oidc.client-id", "", "The OIDC client ID, see https://tools.ietf.org/html/rfc6749#section-2.3.")
+	cmd.Flags().StringVar(&cfg.oidc.audience, "oidc.audience", "", "The audience for whom the access token is intended, see https://openid.net/specs/openid-connect-core-1_0.html#IDToken.")
 
 	return cmd
+}
+
+func runLogin(ctx context.Context, logger log.Logger, cfg loginConfig) error {
+	provider, err := oidc.NewProvider(ctx, cfg.oidc.issuerURL)
+	if err != nil {
+		return fmt.Errorf("constructing oidc provider: %w", err)
+	}
+
+	ccc := clientcredentials.Config{
+		ClientID:     cfg.oidc.clientID,
+		ClientSecret: cfg.oidc.clientSecret,
+		TokenURL:     provider.Endpoint().TokenURL,
+		Scopes:       []string{"openid", "offline_access"},
+	}
+
+	if cfg.oidc.audience != "" {
+		ccc.EndpointParams = url.Values{
+			"audience": []string{cfg.oidc.audience},
+		}
+	}
+
+	tkn, err := ccc.Token(ctx)
+	if err != nil {
+		return fmt.Errorf("fetching token: %w", err)
+	}
+
+	conf, err := config.Read()
+	if err != nil {
+		return err
+	}
+
+	if _, ok := conf.APIs[config.APIName(cfg.api)]; !ok {
+		apiURL, err := url.Parse(cfg.api)
+		if err != nil {
+			return fmt.Errorf("%s is not a valid URL or existing api name", cfg.api)
+		}
+
+		cfg.api = apiURL.Host
+
+		if err := conf.AddAPI(config.APIName(cfg.api), apiURL.String()); err != nil {
+			return fmt.Errorf("adding new api: %w", err)
+		}
+	}
+
+	return conf.AddTenant(
+		config.TenantName(cfg.tenant),
+		config.APIName(cfg.api),
+		cfg.tenant,
+		config.OIDCConfig{
+			AccessToken:  tkn.AccessToken,
+			RefreshToken: tkn.RefreshToken,
+
+			Audience:     cfg.oidc.audience,
+			ClientID:     cfg.oidc.clientID,
+			ClientSecret: cfg.oidc.clientSecret,
+			IssuerURL:    cfg.oidc.issuerURL,
+		},
+	)
 }
